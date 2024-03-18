c2b04a297d224a267c6b1a85ef5f6f9c62cb9e11
Tomas Birbe <tomas.birbe@gmail.com>
2024-03-18 14:41:02 -0300

    make a basic shoplist

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,101 @@
-import Image from "next/image";
+'use client'
+
+import { Button } from "@/components/ui/button";
+import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { useState } from "react";
+
+type Product = {
+	name: string;
+	price: number;
+	amount: number;
+}
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+	const [products, setProducts] = useState<Array<Product>>([])
+	const [IsModalOpen, setIsModalOpen] = useState(false)
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+	function addProduct(event: React.FormEvent<HTMLFormElement>) {
+		event.preventDefault()
+		const form = event.currentTarget
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+		const formData = new FormData(form)
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+		const { name, price, amount } = {
+			name: formData.get('name')?.toString().trim().toLocaleLowerCase(),
+			price: formData.get('price')?.toString().trim().toLocaleLowerCase(),
+			amount: formData.get('amount')?.toString().trim().toLocaleLowerCase(),
+		}
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
+		if (name && price && amount) {
+			const exists = products.some((product) => product.name === name)
+			if(!exists) {
+				setProducts((products) => [...products, { name, price: Number(price), amount: Number(amount) }])
+				setIsModalOpen(false)
+			}
+		}
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+	}
+
+	function toggleModal() {
+		setIsModalOpen((isModalOpen) => !isModalOpen)
+	}
+
+	function removeProduct(productToRemove: Product['name']) {
+		setProducts((products) => products.filter((product) => product.name !== productToRemove)) 
+	}
+
+	return (
+		<main className="w-[800px] min-h-screen mx-auto grid grid-rows-[1fr_auto] justify-items-center pt-4">
+			{
+				products.length > 0 ?
+					<ul className="w-full flex flex-col gap-4">
+						{products.map((product) => <li key={product.name} className="capitalize bg-[whitesmoke] rounded-[6px] py-6 px-4 flex justify-between">
+							<div>
+								<p>{product.name}</p>
+								<p className="opacity-70">{product.amount}</p>
+							</div>
+							<div>
+								<Button variant='ghost' onClick={() => removeProduct(product.name)}>X</Button>
+							</div>
+						</li>)}
+					</ul>
+					: <h1 className="py-52 opacity-50">Esta lista esta vacia 🙈</h1>
+			}
+			<div className="w-full flex justify-between items-center flex-cols py-4 bg-[whitesmoke] px-4 rounded-[6px]">
+				<p>Total</p>
+				<Dialog open={IsModalOpen} onOpenChange={toggleModal}>
+					<DialogTrigger className="w-[40px] h-[40px]">+</DialogTrigger>
+					<DialogContent className="w-[300px]">
+						<DialogHeader>
+							<DialogTitle>Agregar producto</DialogTitle>
+						</DialogHeader>
+						<form className="flex flex-col gap-4" id="add-product" onSubmit={addProduct}>
+							<div className="flex flex-col gap-2">
+								<Label htmlFor="name">Nombre del producto</Label>
+								<Input id="name" name="name" />
+							</div>
+							<div className="flex flex-col gap-2">
+								<Label htmlFor="price">Precio del producto</Label>
+								<Input name="price" id="price" type="number" />
+							</div>
+							<div className="flex flex-col gap-2">
+								<Label htmlFor="amount">Cantidad del producto</Label>
+								<Input id='amount' name='amount' type="number" />
+							</div>
+						</form>
+						<DialogFooter>
+							<Button className="w-full" type="submit" form="add-product">Guardar</Button>
+						</DialogFooter>
+					</DialogContent>
+				</Dialog>
+
+				<p>$500000</p>
+			</div>
+
+
+
+		</main>
+	);
 }
